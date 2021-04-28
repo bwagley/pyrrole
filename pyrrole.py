ad2ad333814cfe5cf3e81d0d81d16aa983dcdd03
Ben Wagley <bwagley@mymail.mines.edu>
2021-04-28 11:35:45 -0600

    Rebased to python. basic midi input works!

---
+++ pyrrole.py
@@ -0,0 +1,53 @@
+import pyaudio
+import numpy as np
+import sys
+import pygame, pygame.midi
+import os, signal
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+
+p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
+
+def sineWave(freq, duration):
+    t = np.linspace(0,duration, duration*RATE)
+    data = np.cos(2*np.pi*freq*t)*127.5
+    return data.astype(np.int16)
+
+def playTone(freq):
+    
+    wave = sineWave(freq, 1)
+    stream.write(wave)
+
+def main():
+    pygame.init()
+    pygame.midi.init()
+
+    inp = pygame.midi.Input(3)
+
+    midiKey = []
+    childKey = {}
+
+    for i in range(127):
+        midiKey.append((440 / 32) * (2 ** ((i - 9) / 12)))
+        childKey[midiKey[i]] = 0;
+
+    while True:
+        if inp.poll():
+            mlist = inp.read(1000)
+            for i in range(len(mlist)):
+                toPlay = midiKey[mlist[i][0][1]]
+                #if mlist[i][0][2] == 0:
+                ##   os.kill(childKey[toPlay], signal.SIGTERM)
+                #    childKey[toPlay] = 0
+                #    continue
+                if childKey[toPlay] == 0:
+                    playTone(toPlay)
+                else: 
+                    exit()
+        pygame.time.wait(10)
+
+main()

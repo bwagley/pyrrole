6cfbb38d678db7bccc02651e19b7f80264d8b4ac
Ben Wagley <bwagley@mymail.mines.edu>
2021-05-03 10:42:53 -0600

    Finished enough for the class

--- pyrrole.py
+++ pyrrole.py
@@ -1,19 +1,33 @@
+#!/bin/python
+
+#Pyrrole, a basic audio synthesis tool
+#Benjamin Wagley - For HASS327 Synth Project
+#Designed for use in conjunction with Echo Circuit designed by Elijah Mt. Castle
+#Requires pyaudio for audio output, pygame for midi input.
+
 import pyaudio
 import numpy as np
 import sys
 import pygame, pygame.midi
 import os, signal
 
+#Set initilization parameters for pyaudio
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 #AMP = 2*127.5
 AMP = 2**12
+#AMP = 2**16
 
+argc = len(sys.argv)
+argv = sys.argv
+
+#Create a pyaudio output stream
 p = pyaudio.PyAudio()
 stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
 
+#Define our waveform functions, using numpy trig functions.
 def sineWave(freq, duration):
     t = np.linspace(0,duration, int(duration*RATE))
     data = np.sin(2*np.pi*freq*t)*AMP
@@ -32,6 +46,7 @@ def sawWave(freq, duration):
 def squareWave(freq, duration):
     data = sineWave(freq, duration)
     for i in range(len(data)):
+        #We "fake" a square wave by using a given sine wave input
         if data[i] < 0:
             data[i] = -AMP
         else:
@@ -39,6 +54,7 @@ def squareWave(freq, duration):
 
     return data
 
+#Create a wave of using a wave function, or a given frequency and duration. 
 def playWave(freq, duration, func):
     period = 1 / freq
     n = duration / period
@@ -46,35 +62,56 @@ def playWave(freq, duration, func):
     return func(freq, duration)
 
 
-def playTone(freq):
-    
-    wave = playWave(freq, 1, sawWave)
+#Play a wave of a given shape (using above playWave function to build wave)
+def playTone(freq, shape):
+    wave = playWave(freq, 1, shape)
     stream.write(wave)
 
 def main():
+    #We have an array of wave shapes so that we can switch through them
+    waveShapes = [sineWave, squareWave, triWave, sawWave]
+    shape = 0
+    if argc == 1:
+        print("No Wave Shape Given, Setting to Sine Wave")
+    else:
+        print(argv[1])
+        if argv[1] == "sine":
+            shape = 0
+        elif argv[1] == "square":
+            shape = 1
+        elif argv[1] == "triangle":
+            shape = 2
+        elif argv[1] == "saw":
+            shape = 3
+        else:
+            print("Invalid Wave Shape Given, please select sine, square, triangle, or saw.\nDefaulting to sine.")
+
+    #Init pygame for midi input
     pygame.init()
     pygame.midi.init()
 
     inp = pygame.midi.Input(3)
 
     midiKey = []
-    childKey = {}
 
+    #Populate an array of frequency for 128 midi keys.
     for i in range(127):
         midiKey.append((440 / 32) * (2 ** ((i - 9) / 12)))
-        childKey[midiKey[i]] = 0;
 
     while True:
         if inp.poll():
             mlist = inp.read(1000)
             for i in range(len(mlist)):
+                #If non-key midi input, change the wave shape
+                if mlist[i][0][0] == 176:
+                    shape = (shape + 1) % 4
+                    print(waveShapes[shape])
+                    continue
+                #Otherwise, get the frequenct to play
                 toPlay = midiKey[mlist[i][0][1]]
-                #if mlist[i][0][2] == 0:
-                ##   os.kill(childKey[toPlay], signal.SIGTERM)
-                #    childKey[toPlay] = 0
-                #    continue
+                #Make sure this is a midi keypress, not release
                 if mlist[i][0][2] != 0:
-                    playTone(toPlay)
+                    playTone(toPlay, waveShapes[shape])
         pygame.time.wait(10)
 
 main()
